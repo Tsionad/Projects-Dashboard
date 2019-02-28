848c557aab7dbc6d1e52656c8583069cc5406bf5
Tsion Adefres <tadefres@gmail.com>
2019-02-28 02:07:41 -0800

    drag & drop feature

--- src/components/projects/ProjectsList.jsx
+++ src/components/projects/ProjectsList.jsx
@@ -1,52 +1,71 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import _ from 'lodash';
+import React from 'react'
+import PropTypes from 'prop-types'
+import _ from 'lodash'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 
-import Project from './Project';
-import defaultIcon from '../../assets/defaultProjectIcon_2x.png';
-import styles from './Projects.css';
-
+import Project from './Project'
+import defaultIcon from '../../assets/defaultProjectIcon_2x.png'
+import styles from './Projects.css'
 
 const ProjectsList = ({
   projects,
   newProject,
   updateProject,
   addProject,
+  onDragOver,
   onDeleteIconClick,
-  handleProjectTitle,
+  handleProjectTitle
 }) => {
-  const renderList = _.map(projects, project => (
+  const renderList = _.map(projects, (project, index) => (
     <Project
-      key={project.id}
+      id={project.id}
+      index={index}
       project={project}
       addProject={addProject}
       handleProjectTitle={handleProjectTitle}
       onDeleteIconClick={onDeleteIconClick}
+      onDragOver={onDragOver}
       updateProject={updateProject}
     />
-  ));
+  ))
 
   return (
-    <div className="projects" style={styles}>
-      {newProject && (
-        <div className="project">
-          <img className="project-icon'" src={defaultIcon} alt="project icon" />
-          <form onSubmit={addProject}>
-            <input
-              className="project-input"
-              type="text"
-              name="projectTitle"
-              placeholder="Name your project"
-              onChange={handleProjectTitle}
+    <DragDropContext onDragEnd={onDragOver}>
+      <div className="projects" style={styles}>
+        {newProject && (
+          <div className="project">
+            <img
+              className="project-icon'"
+              src={defaultIcon}
+              alt="project icon"
             />
-            <input type="submit" style={{ display: 'none' }} />
-          </form>
-        </div>
-      )}
-      {renderList}
-    </div>
-  );
-};
+            <form onSubmit={addProject}>
+              <input
+                className="project-input"
+                type="text"
+                name="projectTitle"
+                placeholder="Name your project"
+                onChange={handleProjectTitle}
+              />
+              <input type="submit" style={{ display: 'none' }} />
+            </form>
+          </div>
+        )}
+        <Droppable droppableId="project-dnd-container">
+          {provided => (
+            <div
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}>
+              {renderList}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  )
+}
 
 ProjectsList.propTypes = {
   projects: PropTypes.array,
@@ -55,6 +74,7 @@ ProjectsList.propTypes = {
   handleProjectTitle: PropTypes.func.isRequired,
   addProject: PropTypes.func.isRequired,
   updateProject: PropTypes.func.isRequired,
-};
+  onDragOver: PropTypes.func.isRequired
+}
 
-export default ProjectsList;
+export default ProjectsList
